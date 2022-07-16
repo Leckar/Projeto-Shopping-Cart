10447010d2ac32a984022b78b7ea347e9d1a6bd3
Leckar <gusleckar@gmail.com>
2022-07-16 19:26:24 -0300

    completed up to requisite 5

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,7 @@
 const fetchItem = async (id) => {
-  console.log(id);
   if (!id) throw new Error('You must provide an url');
   const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const json = await response.json();
-  console.log(json);
   return json;
 };
 
